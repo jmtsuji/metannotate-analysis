71285ff4fc7be8f2c9fedfacb1339a608fa89cf7
Jackson M. Tsuji <jmtsuji@uwaterloo.ca>
2017-10-12 05:46:02 -0400

    Add files via upload
    Minor revisions to header and user variable

--- metannotate_barplots.R
+++ metannotate_barplots.R
@@ -1,10 +1,7 @@
 # MetAnnotate barplot generator
-# By Jackson M. Tsuji
-# Created ~ Mar. 2017
-# Last updated: Oct 10th, 2017
-# Version: 3.0.0
-# Usage note: one of the items in your annotation file MUST be the rpoB gene, currently.
-
+# By Jackson M. Tsuji (Neufeld Lab PhD student)
+# Under the BSD 3 License: https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2017, Jackson M. Tsuji
 
 #######################################
 ##### User variables ##################
@@ -25,16 +22,16 @@ tax_rank_to_plot <- "Family"
 top_number_to_plot <- 0.01 # If < 1, then plot all taxa with at least this relative abundance in the community.
 #                            If > 1 (e.g., 10), then plot the top ___ (e.g., 10) taxa for each gene
 
+### Options for output:
+write_tables <- TRUE # Print off summary tables?
+printPDF <- TRUE
+output_name_general <- "171011_barplot" # general name of output files to append to
+
 ### Advanced features for making custom plots
 # **NOTE: the basic plot settings (above) MUST match those from when the template file was created!
 print_custom_plot_template <- FALSE # Prints a template for the user to generate a plot with their own colours, then EXITS
 print_custom_plot <- TRUE # ONLY works if you have already run print_custom_plot_template and filled in the table
 custom_plot_template_filename <- "171011_barplot_05_custom_plot_template_Family_0.01_FILLED.tsv" # Required for custom_plot_template_filename
-
-### Options for output:
-write_tables <- TRUE # Print off summary tables?
-printPDF <- TRUE
-output_name_general <- "171011_barplot" # general name of output files to append to
 
 #######################################
 #######################################
